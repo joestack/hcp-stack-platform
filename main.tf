4564a24ccaecbee574aeb33e89509e53b40e0e8a
JoeStack <joern@hashicorp.com>
2023-12-14 13:35:23 +0100

    cleanup 3

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "hcp" {
   client_secret = var.client_secret
 }
 
-resource "hcp_boundary_cluster" "hcp_demo" {
+resource "hcp_boundary_cluster" "hcp_platform" {
   count      = var.boundary_enabled ? 1 : 0
   cluster_id = "${var.name}-boundary-cluster"
   tier       = var.boundary_tier
@@ -11,7 +11,7 @@ resource "hcp_boundary_cluster" "hcp_demo" {
   password   = var.boundary_password
 }
 
-resource "hcp_hvn" "hcp_demo" {
+resource "hcp_hvn" "hcp_platform" {
   count          = anytrue([var.hvn_enabled, var.vault_enabled, var.consul_enabled]) ? 1 : 0
   hvn_id         = "${var.name}-demo-hvn"
   cloud_provider = var.hvn_provider
@@ -19,29 +19,29 @@ resource "hcp_hvn" "hcp_demo" {
   cidr_block     = var.hvn_cidr_block
 }
 
-resource "hcp_vault_cluster" "hcp_demo" {
+resource "hcp_vault_cluster" "hcp_platform" {
   count           = var.vault_enabled ? 1 : 0
   cluster_id      = "${var.name}-vault-cluster"
-  hvn_id          = hcp_hvn.hcp_demo[0].hvn_id
+  hvn_id          = hcp_hvn.hcp_platform[0].hvn_id
   public_endpoint = var.vault_public_endpoint
   tier            = var.vault_tier
 }
 
-resource "hcp_vault_cluster_admin_token" "hcp_demo" {
+resource "hcp_vault_cluster_admin_token" "hcp_platform" {
   count      = var.vault_enabled ? 1 : 0
-  cluster_id = hcp_vault_cluster.hcp_demo[0].cluster_id
-  #cluster_id = element(hcp_vault_cluster.hcp_demo.*.cluster_id, count.index)
+  cluster_id = hcp_vault_cluster.hcp_platform[0].cluster_id
+  #cluster_id = element(hcp_vault_cluster.hcp_platform.*.cluster_id, count.index)
 }
 
-resource "hcp_consul_cluster" "hcp_demo" {
+resource "hcp_consul_cluster" "hcp_platform" {
   count           = var.consul_enabled ? 1 : 0
   cluster_id      = "${var.name}-consul-cluster"
-  hvn_id          = hcp_hvn.hcp_demo[0].hvn_id
+  hvn_id          = hcp_hvn.hcp_platform[0].hvn_id
   public_endpoint = var.consul_public_endpoint
   tier            = var.consul_tier
 }
 
-resource "hcp_consul_cluster_root_token" "hcp_demo" {
+resource "hcp_consul_cluster_root_token" "hcp_platform" {
   count      = var.consul_enabled ? 1 : 0
-  cluster_id = hcp_consul_cluster.hcp_demo[0].cluster_id
+  cluster_id = hcp_consul_cluster.hcp_platform[0].cluster_id
 }
--- outputs.tf
+++ outputs.tf
@@ -1,78 +1,78 @@
 output "boundary_cluster_url" {
   value = var.boundary_enabled ? (
-    hcp_boundary_cluster.hcp_demo[0].cluster_url
+    hcp_boundary_cluster.hcp_platform[0].cluster_url
   ) : null
 }
 
 output "vault_cluster_public_url" {
   value = var.vault_enabled ? (
-    hcp_vault_cluster.hcp_demo[0].vault_public_endpoint_url
+    hcp_vault_cluster.hcp_platform[0].vault_public_endpoint_url
   ) : null
 }
 
 output "vault_cluster_private_url" {
   value = var.vault_enabled ? (
-    hcp_vault_cluster.hcp_demo[0].vault_private_endpoint_url
+    hcp_vault_cluster.hcp_platform[0].vault_private_endpoint_url
   ) : null 
 }
 
 output "vault_namespace" {
   value = var.vault_enabled ? (
-  hcp_vault_cluster.hcp_demo[0].namespace
+  hcp_vault_cluster.hcp_platform[0].namespace
   ) : null
 }
 
 output "vault_admin_token" {
   value = var.vault_enabled ? (
-  hcp_vault_cluster_admin_token.hcp_demo[0].token
+  hcp_vault_cluster_admin_token.hcp_platform[0].token
   ) : null
   sensitive = true
 }
 
 output "consul_root_token" {
   value = var.consul_enabled ? (
-    hcp_consul_cluster_root_token.hcp_demo[0].secret_id
+    hcp_consul_cluster_root_token.hcp_platform[0].secret_id
   ) : null
   sensitive = true
 }
 
 output "consul_url" {
   value = var.consul_enabled ? (
-    hcp_consul_cluster.hcp_demo[0].public_endpoint ? (
-    hcp_consul_cluster.hcp_demo[0].consul_public_endpoint_url
+    hcp_consul_cluster.hcp_platform[0].public_endpoint ? (
+    hcp_consul_cluster.hcp_platform[0].consul_public_endpoint_url
     ) : (
-    hcp_consul_cluster.hcp_demo[0].consul_private_endpoint_url
+    hcp_consul_cluster.hcp_platform[0].consul_private_endpoint_url
   )
   ) : null
 }
 
 output "client_ca_file" {
   value = var.consul_enabled ? (
-    hcp_consul_cluster.hcp_demo[0].consul_ca_file
+    hcp_consul_cluster.hcp_platform[0].consul_ca_file
   ) : null
 }
 
 output "client_config_file" {
   value = var.consul_enabled ? (
-    hcp_consul_cluster.hcp_demo[0].consul_config_file
+    hcp_consul_cluster.hcp_platform[0].consul_config_file
   ) : null
 }
 
 output "consul_version" {
   value = var.consul_enabled ? (
-    hcp_consul_cluster.hcp_demo[0].consul_version
+    hcp_consul_cluster.hcp_platform[0].consul_version
   ) : null
 }
 
 output "consul_datacenter" {
   value = var.consul_enabled ? (
-    hcp_consul_cluster.hcp_demo[0].datacenter
+    hcp_consul_cluster.hcp_platform[0].datacenter
   ) : null
 }
 
 output "hcp_consul_cluster_id" {
   value       = var.consul_enabled ? (
-    hcp_consul_cluster.hcp_demo[0].cluster_id
+    hcp_consul_cluster.hcp_platform[0].cluster_id
   ) : null
   description = "HCP Consul ID"
 }
@@ -80,9 +80,9 @@ output "hcp_consul_cluster_id" {
 output "hvn" {
   value = anytrue([var.hvn_enabled, var.vault_enabled, var.consul_enabled]) ? (
     {
-    hvn_id     = hcp_hvn.hcp_demo[0].hvn_id
-    self_link  = hcp_hvn.hcp_demo[0].self_link
-    cidr_block = hcp_hvn.hcp_demo[0].cidr_block
+    hvn_id     = hcp_hvn.hcp_platform[0].hvn_id
+    self_link  = hcp_hvn.hcp_platform[0].self_link
+    cidr_block = hcp_hvn.hcp_platform[0].cidr_block
     }
   ) : null
 }
@@ -98,49 +98,49 @@ output "hcp_client_secret" {
 }
 
 # output "created_at" {
-#   value = hcp_boundary_cluster.hcp_demo.created_at
+#   value = hcp_boundary_cluster.hcp_platform.created_at
 # }
 
 # output "id" {
-#   value = hcp_boundary_cluster.hcp_demo.id
+#   value = hcp_boundary_cluster.hcp_platform.id
 # }
 
 # output "state" {
-#   value = hcp_boundary_cluster.hcp_demo.state
+#   value = hcp_boundary_cluster.hcp_platform.state
 # }
 
 # output "tier" {
-#   value = hcp_boundary_cluster.hcp_demo.tier
+#   value = hcp_boundary_cluster.hcp_platform.tier
 # }
 
 # output "hvn_id" {
-#   value = hcp_hvn.hcp_demo.id
+#   value = hcp_hvn.hcp_platform.id
 # }
 
 # output "hvn_cidr_block" {
-#   value = hcp_hvn.hcp_demo.cidr_block
+#   value = hcp_hvn.hcp_platform.cidr_block
 # }
 
 # output "hvn_cloud_provider" {
-#   value = hcp_hvn.hcp_demo.cloud_provider
+#   value = hcp_hvn.hcp_platform.cloud_provider
 # }
 
 # output "hvn_organization_id" {
-#   value = hcp_hvn.hcp_demo.organization_id
+#   value = hcp_hvn.hcp_platform.organization_id
 # }
 
 # output "hvn_provider_account_id" {
-#   value = hcp_hvn.hcp_demo.provider_account_id
+#   value = hcp_hvn.hcp_platform.provider_account_id
 # }
 
 # output "hvn_region" {
-#   value = hcp_hvn.hcp_demo.region
+#   value = hcp_hvn.hcp_platform.region
 # }
 
 # output "hvn_self_link" {
-#   value = hcp_hvn.hcp_demo.self_link
+#   value = hcp_hvn.hcp_platform.self_link
 # }
 
 # output "hvn_state" {
-#   value = hcp_hvn.hcp_demo.state
+#   value = hcp_hvn.hcp_platform.state
 # }
